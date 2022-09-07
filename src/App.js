9040f240ff90a1ad56e19d5fe46c7d769a0d2bd7
“kashsan-jp” <kashsan_jp@yahoo.co.jp>
2022-09-08 00:25:21 +0900

    kash

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <h2>Hello React app with AWS</h2>
+       <h2>Hello React app with AWS Kashsan</h2>
       </header>
     </div>
   );
